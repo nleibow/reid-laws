523058ee960d721915ed8aa5c230e6e8b1e2fae9
Taylor Laine <taylor.laine@gyro.com>
2018-04-23 12:06:27 -0600

    fixes scroll btn from hero

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -18,17 +18,18 @@ const Nav = () => {
         document.querySelector('a[href="' + elementsReverse[elementsReverse.length - 2] + '"]').classList.remove("active");
         nav.classList.remove("stuck")
     }
+
 // Adds active state on scroll
     document.addEventListener("scroll", () => {
         const scrollLocation = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
         const nav = document.querySelector(".navigation");
 
-        for (let i = 0; i < elementsReverse.length ; i++) {
+        for (let i = 0; i < elementsReverse.length; i++) {
             let el = document.querySelector(elementsReverse[i]);
             let elementScrollLocation = el.offsetTop - scrollLocation;
             if (elementScrollLocation < 200 && (-el.offsetHeight) < elementScrollLocation - 100) {
-                for (let i = 0; i < elementsReverse.length ; i++) {
-                    window.console.log( elementsReverse[i]);
+                for (let i = 0; i < elementsReverse.length; i++) {
+                    window.console.log(elementsReverse[i]);
                     window.console.log(-el.offsetHeight, "-el offset");
                     window.console.log(elementScrollLocation - 100, "-scrollLocation");
                     document.querySelector('nav a[href="' + elementsReverse[i] + '"]').classList.remove("active");
@@ -36,7 +37,7 @@ const Nav = () => {
                 document.querySelector('nav a[href="' + elementsReverse[i] + '"]').classList.add("active");
             }
 
-            if(elementScrollLocation > 90) {
+            if (elementScrollLocation > 90) {
                 document.querySelector('nav a[href="' + elementsReverse[2] + '"]').classList.remove("active");
             }
         }
@@ -52,7 +53,7 @@ const Nav = () => {
     // Smooth scrolls page on nav click
     [].forEach.call(document.querySelectorAll("nav a"), (elem) => {
         elem.addEventListener('click', (e) => {
-            if (!(elem.getAttribute('href').indexOf('http') > -1)) {
+            if (!(elem.getAttribute('href').indexOf('http') > -1) && !(elem.getAttribute('href').indexOf('pdf') > -1)) {
                 e.preventDefault();
                 toggleNav();
                 const scrollToElement = document.querySelector(elem.getAttribute('href'));
@@ -67,13 +68,13 @@ const Nav = () => {
 
     function toggleNav() {
         let nav = document.getElementById('nav');
-        if(window.innerWidth < 760) {
-            if (!nav.classList.contains("show")){
+        if (window.innerWidth < 760) {
+            if (!nav.classList.contains("show")) {
                 nav.classList.add('show');
                 document.body.style.overflow = 'hidden';
                 document.getElementById('hamburger').classList.add("hide");
                 document.getElementById('close-x').classList.add("show");
-            }else {
+            } else {
                 nav.classList.remove('show');
                 document.body.style.overflow = 'auto';
                 document.getElementById('hamburger').classList.remove("hide");
@@ -87,7 +88,7 @@ const Nav = () => {
 
     //Resize for mobile and desktop nav
     window.addEventListener("resize", () => {
-        if(window.innerWidth < 768) {
+        if (window.innerWidth < 768) {
             document.getElementById('nav').classList.remove('show');
             document.getElementById('hamburger').style.display = '';
             document.body.style.overflow = 'auto';
@@ -96,16 +97,6 @@ const Nav = () => {
             document.body.style.overflow = 'auto';
         }
     });
-setTimeout( () => {
-    const navBtn2 = document.getElementById('calcBtn2');
-    const navBtn1 = document.getElementById('calcBtn1');
-    navBtn1.addEventListener('click',  ()=> {
-        window.console.log('works')
-    });
-    navBtn2.addEventListener('click',  ()=> {
-        window.console.log('works')
-    });
-}, 300)
 }
 
 
